6779d2c1dead9d7e63c7f31fd34f3a4c9d28261a
Dean M <dean.m.miranda@gmail.com>
2023-08-30 19:56:05 -0400

    Balint - Cleaned Errors and lint warnings

--- Develop/client/src/utils/auth.js
+++ Develop/client/src/utils/auth.js
@@ -1,7 +1,5 @@
 // use this to decode a token and get the user's information out of it
 import decode from 'jwt-decode';
-
-// create a new class to instantiate for a user
 class AuthService {
   // get user data
   getProfile() {
@@ -46,4 +44,5 @@ class AuthService {
   }
 }
 
-export default new AuthService();
+const authServiceInstance = new AuthService();
+export default authServiceInstance;
